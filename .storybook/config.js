04418b8aedf8352082783b5d654b99e29f4eac9a
zlatan <zlatan.pham@gmail.com>
2019-08-12 23:03:59 +0700

    test outside storybook root

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /\.stories\.js$/);
+const stories = require.context('../stories', true, /\.stories\.js$/);
+const components = require.context('../ui', true, /\.stories\.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  stories.keys().forEach(filename => stories(filename));
+  components.keys().forEach(filename => components(filename));
 }
 
 configure(loadStories, module);
