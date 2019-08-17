6cc619bf00ce6e426a71cef5deb33c1ac693173d
zlatan <zlatan.pham@gmail.com>
2019-08-17 14:59:32 +0700

    compile storybook with antd

--- storybook/.storybook/webpack.config.js
+++ storybook/.storybook/webpack.config.js
@@ -1,11 +1,59 @@
+const path = require('path');
+// const webpack = require('webpack');
+const fs = require('fs');
+const lessToJs = require('less-vars-to-js');
+const themeVars = lessToJs(
+  fs.readFileSync(path.join(__dirname, '../../config/antTheme.less'), 'utf8'),
+);
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
     options: {
       presets: [['react-app', { flow: false, typescript: true }]],
+      plugins: [
+        [
+          'import',
+          {
+            libraryName: 'lodash',
+            libraryDirectory: '',
+            camel2DashComponentName: false,
+          },
+          'lodash',
+        ],
+        [
+          'import',
+          {
+            libraryName: 'antd',
+            libraryDirectory: 'es',
+            style: true,
+          },
+          'antd',
+        ],
+      ],
     },
   });
+
+  config.module.rules.push({
+    test: /\.less$/,
+    loaders: [
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'less-loader',
+        options: {
+          modifyVars: themeVars,
+          javascriptEnabled: true,
+        },
+      },
+    ],
+    include: [
+      path.join(__dirname, '../config'),
+      /[\\/]node_modules[\\/].*antd/,
+    ],
+  });
+
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
---
+++ ui/src/Button.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { Button } from './Button';
+
+storiesOf('Button my', module).add('all', () => <Button>my button</Button>);
